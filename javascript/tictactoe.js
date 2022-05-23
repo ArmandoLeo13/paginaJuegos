d656d738717ac79e910bc825405675e3c33929b4
Armando Machado <armandomachado13@yahoo.com>
2022-05-23 19:34:47 -0300

    Version 1

---
+++ javascript/tictactoe.js
@@ -0,0 +1,38 @@
+let tablero = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+];
+
+class Jugador{
+    constructor(){
+        this.jugado = false;
+        this.id = id;
+    }
+}
+
+class espacio{
+    constructor(id){
+        this.jugado = false;
+        this.id = id;
+    }
+    agregarX(espacio){
+
+    }
+    agregar0(espacio){
+
+    }
+}
+
+const jugador1 = new Jugador();
+const jugador2 = new Jugador();
+
+const espacio1 = new espacio("uno");
+const espacio2 = new espacio("dos");
+const espacio3 = new espacio("tres");
+const espacio4 = new espacio("cuatro");
+const espacio5 = new espacio("cinco");
+const espacio6 = new espacio("seis");
+const espacio7 = new espacio("siete");
+const espacio8 = new espacio("ocho");
+const espacio9 = new espacio("nueve");
